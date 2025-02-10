c2330116a99f8d94a2375dd21828aac3589c7477
Julia Pierson <juliapiersonnyc@gmail.com>
2025-02-10 16:25:33 -0500

    add x logo

--- nightclub-wrapped-app/src/XShareButton/index.js
+++ nightclub-wrapped-app/src/XShareButton/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import xIcon from "../../public/xlogo.png"
 
 const XShareButton = ({ text, url }) => {
   const xShareUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}&url=${encodeURIComponent(url)}`;
@@ -10,7 +11,12 @@ const XShareButton = ({ text, url }) => {
       rel="noopener noreferrer"
       className="flex items-center justify-center bg-[#000000] text-blue font-medium px-4 py-2 rounded-lg"
     >
-        Share on X
+        Share on
+        <img
+        src="/xlogo.png"
+        alt="X Logo"
+        style={{ width: '20px', height: '20px', marginRight: '8px', marginLeft: '8px' }}
+      />
     </a>
   );
 };
--- nightclub-wrapped-app/src/app/player/page.js
+++ nightclub-wrapped-app/src/app/player/page.js
@@ -12,11 +12,11 @@ export default function PlayerPage() {
     return (
         <main className="flex justify-center min-h-screen flex-col items-center bg-gradient-to-b from-slate-800 to-gray-900">
             <h1 className="mb-6 md:mb-8 text-5xl md:text-6xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-600 px-2 py-3">{tag}'s Nightclub Wrapped</h1>
-            <XShareButton className="flex items-center justify-center bg-[#000000] text-blue font-medium px-4 py-2 rounded-lg" text="I just got my Nightclub Wrapped for 2024!" url="http://localhost:3002" />
-            <br />
             <NightclubStatsImage tag={tag}/>
             <br />
             <NightclubRedemptionStatsImage tag={tag} />
+            <XShareButton className="flex items-center justify-center bg-[#000000] text-blue font-medium px-4 my-2 py-2 rounded-lg" text="I just got my Nightclub Wrapped for 2024!" url="http://localhost:3002" />
+            <br />
         </main>
     )
 } 
